1c650aeb17b83984ee3f8daea446ac99ade83aaa
Latif Altay <latifaltay00@gmail.com>
2025-01-03 22:44:44 +0300

    Database First scaffolding has been completed

---
+++ DataProtection.Web/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataProtection.Web.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string? Color { get; set; }
+
+    public int? ProductCategoryId { get; set; }
+}
