d6570aa1aeb2deb52efe03cccf9479ecaf1682b5
Latif Altay <latifaltay00@gmail.com>
2025-01-04 04:20:43 +0300

    ip control middleware has been done

---
+++ WhiteBlackList.Web/MiddleWares/IPSafeMiddleWare.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net;
+
+namespace WhiteBlackList.Web.MiddleWares
+{
+    public class IPSafeMiddleWare
+    {
+
+        private readonly RequestDelegate _next;
+        private readonly IPList _iPList;
+
+        public IPSafeMiddleWare(RequestDelegate next, IOptions<IPList> ipList)
+        {
+            _next = next;
+            _iPList = ipList.Value;
+
+        }
+
+        public async Task Invoke(HttpContext context) 
+        {
+            var requestIpAddress = context.Connection.RemoteIpAddress;
+
+            var isWhiteList = _iPList.WhiteList.Where(x => IPAddress.Parse(x).Equals(requestIpAddress)).Any();
+
+            if (!isWhiteList) 
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return;
+            }
+
+            await _next(context);
+
+        }
+
+    }
+}
