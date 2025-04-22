3f45efe8cbd063b35b2a37a121258ba327087082
l.altay <l.altay@nexum.com.tr>
2025-04-23 02:40:52 +0300

    encode has been done

--- XSS.Web/Controllers/HomeController.cs
+++ XSS.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Mvc;
 using XSS.Web.Models;
 
@@ -6,15 +7,22 @@ namespace XSS.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;
+        private HtmlEncoder _htmlEncoder;
+        private JavaScriptEncoder _javascriptEncoder;
+        private UrlEncoder _urlEncoder;
+
+        public HomeController(ILogger<HomeController> logger, HtmlEncoder htmlEncoder, JavaScriptEncoder javaScriptEncoder, UrlEncoder urlEncoder)
         {
             _logger = logger;
+            _htmlEncoder = htmlEncoder;
+            _javascriptEncoder = javaScriptEncoder;
+            _urlEncoder = urlEncoder;
         }
 
         
-        public IActionResult CommentAdd() 
+        public IActionResult CommentAdd()  
         {
             HttpContext.Response.Cookies.Append("Email", "admin@admin.com");
             HttpContext.Response.Cookies.Append("Password", "Admin123");
@@ -31,6 +39,9 @@ namespace XSS.Web.Controllers
         [HttpPost]
         public IActionResult CommentAdd(string name, string comment)
         {
+
+            string encodedName = _urlEncoder.Encode(name);
+
             ViewBag.name = name;
             ViewBag.comment = comment;
 
